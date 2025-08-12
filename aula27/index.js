5c0bea1185f3db9bef6724f5b34aa6bb13257705
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2025-08-12 18:09:38 -0300

    aulas-27-e-28

---
+++ aula27/index.js
@@ -0,0 +1,5 @@
+// (condição) ? 'valor para verdadeiro' : 'valor para falso';
+
+const pontuacaoUsuario = 500;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
+console.log(nivelUsuario);
